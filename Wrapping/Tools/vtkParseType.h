dgobbi/WrapVTK

Add parsing of variadic templates for C++11 support.
This was done by adding opt_ellipsis where it was needed for C++11
(specifically after base class IDs and before declarator IDs), and
then adding opt_ellipsis in a whole bunch of other places in order
to eliminate shift/reduce errors in the parser generation.  Base
class specifiers that are parameter packs are ignored for now.

Wrapping/vtkParseType.h
@@ -78,6 +78,7 @@
  */
 #define VTK_PARSE_TYPEDEF     0x010000000
 #define VTK_PARSE_FRIEND      0x020000000
+#define VTK_PARSE_PACK        0x040000000
 
 /**
  * Mask for removing qualifiers

GerHobbelt/VTK

Change-Id: I86d13da5ed6585a529e07434946b33654a54e9a9

Wrapping/Tools/vtkParseType.h
@@ -79,6 +79,7 @@
  */
 #define VTK_PARSE_TYPEDEF     0x10000000
 #define VTK_PARSE_FRIEND      0x20000000
+#define VTK_PARSE_PACK        0x40000000
 
 /**
  * Mask for removing qualifiers