GerHobbelt/VTK

commit bba39d5b1cec42c4cce0c0272b1bf7b135e392ad

vtkOpenGLRenderUtilities: move Render{Quad,Triangle}
These methods cause vtkOpenGLRenderWindow to include vtk_glew.h (for
GLuint) which must be included before gl.h to work properly. Since this
header gets included in many places, move the affected methods to
another class rather than forcing all users to do the proper inclusion
order dance.

Rendering/External/vtkExternalOpenGLRenderWindow.cxx
@@ -13,6 +13,8 @@
 
 =========================================================================*/
 
+#include "vtk_glew.h"
+
 #include "vtkExternalOpenGLRenderWindow.h"
 #include "vtkObjectFactory.h"
 #include "vtkRendererCollection.h"

Rendering/LICOpenGL2/vtkLineIntegralConvolution2D.cxx
@@ -24,6 +24,7 @@
 #include "vtkMath.h"
 #include "vtkObjectFactory.h"
 #include "vtkOpenGLError.h"
+#include "vtkOpenGLRenderUtilities.h"
 #include "vtkOpenGLRenderWindow.h"
 #include "vtkOpenGLShaderCache.h"
 #include "vtkPainterCommunicator.h"
@@ -717,7 +718,7 @@ public:
       computeBounds[1]*2.0-1.0, computeBounds[3]*2.0-1.0, 0.0f,
       computeBounds[0]*2.0-1.0, computeBounds[3]*2.0-1.0, 0.0f};
 
-    vtkOpenGLRenderWindow::RenderQuad(verts, tcoords,
+    vtkOpenGLRenderUtilities::RenderQuad(verts, tcoords,
       cbo->Program, cbo->VAO);
     vtkOpenGLStaticCheckErrorMacro("failed at RenderQuad");
     }

Rendering/LICOpenGL2/vtkSurfaceLICMapper.cxx
@@ -37,6 +37,7 @@
 #include "vtkOpenGLActor.h"
 #include "vtkOpenGLCamera.h"
 #include "vtkOpenGLError.h"
+#include "vtkOpenGLRenderUtilities.h"
 #include "vtkOpenGLRenderWindow.h"
 #include "vtkOpenGLShaderCache.h"
 #include "vtkPainterCommunicator.h"
@@ -1175,7 +1176,7 @@ public:
       quadTCoords[1]*2.0-1.0, quadTCoords[3]*2.0-1.0, 0.0f,
       quadTCoords[0]*2.0-1.0, quadTCoords[3]*2.0-1.0, 0.0f};
 
-    vtkOpenGLRenderWindow::RenderQuad(verts, tcoords,
+    vtkOpenGLRenderUtilities::RenderQuad(verts, tcoords,
       cbo->Program, cbo->VAO);
     vtkOpenGLStaticCheckErrorMacro("failed at RenderQuad");
   }

Rendering/OpenGL2/CMakeLists.txt
@@ -48,6 +48,7 @@ set(Module_SRCS
   vtkOpenGLPolyDataMapper2D.cxx
   vtkOpenGLProperty.cxx
   vtkOpenGLRenderWindow.cxx
+  vtkOpenGLRenderUtilities.cxx
   vtkOpenGLRenderer.cxx
   vtkOpenGLShaderCache.cxx
   vtkOpenGLTexture.cxx

Rendering/OpenGL2/vtkCocoaRenderWindow.mm
@@ -17,6 +17,7 @@ PURPOSE.  See the above copyright notice for more information.
 #import <Cocoa/Cocoa.h>
 #import "vtkCocoaMacOSXSDKCompatibility.h" // Needed to support old SDKs
 
+#import "vtkOpenGL.h"
 #import "vtkCocoaRenderWindow.h"
 #import "vtkRenderWindowInteractor.h"
 #import "vtkCommand.h"

Rendering/OpenGL2/vtkCompositePolyDataMapper2.cxx
@@ -12,6 +12,8 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+#include "vtk_glew.h"
+
 #include "vtkCompositePolyDataMapper2.h"
 
 #include "vtkCellArray.h"

Rendering/OpenGL2/vtkFrameBufferObject.cxx
@@ -17,6 +17,7 @@
 #include "vtk_glew.h"
 
 #include "vtkObjectFactory.h"
+#include "vtkOpenGLRenderUtilities.h"
 #include "vtkOpenGLRenderWindow.h"
 #include "vtkTextureObject.h"
 #include "vtkPixelBufferObject.h"
@@ -890,7 +891,7 @@ void vtkFrameBufferObject::RenderQuad(int minX, int maxX, int minY, int maxY,
     1.0, 0,
     1.0, maxYTexCoord,
     0, maxYTexCoord};
-  vtkOpenGLRenderWindow::RenderQuad(verts, tcoords, program, vao);
+  vtkOpenGLRenderUtilities::RenderQuad(verts, tcoords, program, vao);
 
   vtkOpenGLCheckErrorMacro("failed after Render");
 

Rendering/OpenGL2/vtkImageProcessingPass.cxx
@@ -13,6 +13,8 @@
 
 =========================================================================*/
 
+#include "vtk_glew.h"
+
 #include "vtkImageProcessingPass.h"
 #include "vtkObjectFactory.h"
 #include <cassert>

commit 3a576d4333762f57bcf997e425972c57ca1f5561

Fix compile error when compiling vtkRenderingExternal
Compiler error introduced in bba39d5b when building
vtkRenderingExternal module with the OpenGL backend.
No vtk_glew.h available for this backend.

Rendering/External/vtkExternalOpenGLRenderWindow.cxx
@@ -13,7 +13,9 @@
 
 =========================================================================*/
 
-#include "vtk_glew.h"
+#ifdef VTK_OPENGL2
+  #include "vtk_glew.h"
+#endif
 
 #include "vtkExternalOpenGLRenderWindow.h"
 #include "vtkObjectFactory.h"