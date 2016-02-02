GerHobbelt/VTK

commit 69c6f7e48828989b6d673760642bfda633270254

Reuse functions from vtksys::SystemTools.
Reusing utility functions from SystemTools to split file name and
extract components from it.

IO/ParallelXML/vtkXMLPDataWriter.cxx
@@ -25,6 +25,7 @@
 #include <algorithm>
 #include <cassert>
 #include <sstream>
+#include <vtksys/SystemTools.hxx>
 
 vtkCxxSetObjectMacro(vtkXMLPDataWriter, Controller, vtkMultiProcessController);
 //----------------------------------------------------------------------------
@@ -330,74 +331,19 @@ void vtkXMLPDataWriter::SplitFileName()
 {
   // Split the FileName into its PathName, FileNameBase, and
   // FileNameExtension components.
-  size_t length = strlen(this->FileName);
-  char* fileName = new char[length+1];
-  strcpy(fileName, this->FileName);
-  char* begin = fileName;
-  char* end = fileName + length;
-  char* s;
 
-#if defined(_WIN32)
-  // Convert to UNIX-style slashes.
-  for (s = begin; s != end; ++s)
-    {
-    if (*s == '\\')
-      {
-      *s = '/';
-      }
-    }
-#endif
+  std::string pathname = vtksys::SystemTools::GetProgramPath(this->FileName);
+  pathname += "/";
+  std::string filename_wo_ext = vtksys::SystemTools::GetFilenameWithoutExtension(this->FileName);
+  std::string ext = vtksys::SystemTools::GetFilenameExtension(this->FileName);
 
-  // Extract the path name up to the last '/'.
   delete [] this->PathName;
-  this->PathName = 0;
-  char* rbegin = end - 1;
-  char* rend = begin - 1;
-  for (s = rbegin; s != rend; --s)
-    {
-    if(*s == '/')
-      {
-      break;
-      }
-    }
-  if (s >= begin)
-    {
-    length = (s-begin) + 1;
-    this->PathName = new char[length+1];
-    strncpy(this->PathName, this->FileName, length);
-    this->PathName[length] = '\0';
-    begin = s+1;
-    }
-
-  // "begin" now points at the beginning of the file name.
-  // Look for the first "." to pull off the longest extension.
-  delete [] this->FileNameExtension;
-  this->FileNameExtension = 0;
-  for (s = begin; s != end; ++s)
-    {
-    if (*s == '.')
-      {
-      break;
-      }
-    }
-  if (s < end)
-    {
-    length = end - s;
-    this->FileNameExtension = new char[length + 1];
-    strncpy(this->FileNameExtension, s, length);
-    this->FileNameExtension[length] = '\0';
-    end = s;
-    }
-
-  // "end" now points to end of the file name.
   delete [] this->FileNameBase;
-  length = end - begin;
-  this->FileNameBase = new char[length+1];
-  strncpy(this->FileNameBase, begin, length);
-  this->FileNameBase[length] = '\0';
+  delete [] this->FileNameExtension;
 
-  // Cleanup temporary name.
-  delete [] fileName;
+  this->PathName = vtksys::SystemTools::DuplicateString(pathname.c_str());
+  this->FileNameBase = vtksys::SystemTools::DuplicateString(filename_wo_ext.c_str());
+  this->FileNameExtension = vtksys::SystemTools::DuplicateString(ext.c_str());
 }
 
 //----------------------------------------------------------------------------

commit 1ffb262f85efe5aa76411120b050647a364f6b58

Handle empty directories (regression introduced by 69c6f7e48).
Fixes regression introduced by 69c6f7e48. If the pathname is empty, we
ended up adding a leading "/" to the filenames instead of treating it
as a path relative to current working directory. Fixed that.

IO/ParallelXML/vtkXMLPDataWriter.cxx
@@ -333,7 +333,12 @@ void vtkXMLPDataWriter::SplitFileName()
   // FileNameExtension components.
 
   std::string pathname = vtksys::SystemTools::GetProgramPath(this->FileName);
-  pathname += "/";
+  // Pathname may be empty if FileName is simply a filename without any leading
+  // "/".
+  if (!pathname.empty())
+    {
+    pathname += "/";
+    }
   std::string filename_wo_ext = vtksys::SystemTools::GetFilenameWithoutExtension(this->FileName);
   std::string ext = vtksys::SystemTools::GetFilenameExtension(this->FileName);
 