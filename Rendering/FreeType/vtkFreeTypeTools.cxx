GerHobbelt/VTK

commit 5dcad9788f710a5a9727cb3c1da34c7db396feb0

vtkTextMapper: fix rendering of empty string
vtkTextMapper improperly renders empty strings. Instead of rendering nothing, as
expected for an empty string, a rectangle is rendered. The rectangle is visible
only when the text property's background is not completely transparent.

This commit fixes the problem at two levels: in vtkFreeTypeTools and in
vtkTextMapper.

vtkFreeTypeTools now checks for an empty string when rendering to an image.
Without this check, rendering an empty string results in the small rectangle.

vtkTextMapper::RenderOverlay() is updated to handle the case of an empty image.
This also serves as a revised solution to #15787:
http://www.vtk.org/Bug/view.php?id=15787

Fixes #16071:
http://www.vtk.org/Bug/view.php?id=16071

Rendering/FreeType/vtkFreeTypeTools.cxx
@@ -1239,6 +1239,14 @@ bool vtkFreeTypeTools::RenderStringInternal(vtkTextProperty *tprop,
     return false;
     }
 
+  if (str.empty())
+    {
+    data->Initialize();
+    textDims[0] = 0;
+    textDims[1] = 0;
+    return true;
+    }
+
   ImageMetaData metaData;
 
   // Setup the metadata cache

Rendering/Core/vtkTextMapper.cxx
@@ -367,7 +367,7 @@ void vtkTextMapper::RenderOverlay(vtkViewport *viewport, vtkActor2D *actor)
   vtkDebugMacro(<<"RenderOverlay called");
 
   vtkRenderer *ren = NULL;
-  if (this->Input)
+  if (this->Input && this->Input[0])
     {
     vtkWindow *win = viewport->GetVTKWindow();
     if (!win)
@@ -394,15 +394,15 @@ void vtkTextMapper::RenderOverlay(vtkViewport *viewport, vtkActor2D *actor)
       info->Set(vtkProp::GeneralTextureUnit(),
         this->Texture->GetTextureUnit());
       }
-    }
 
-  vtkDebugMacro(<<"PolyData::RenderOverlay called");
-  this->Mapper->RenderOverlay(viewport, actor);
+    vtkDebugMacro(<<"PolyData::RenderOverlay called");
+    this->Mapper->RenderOverlay(viewport, actor);
 
-  // clean up
-  if (ren)
-    {
-    this->Texture->PostRender(ren);
+    // clean up
+    if (ren)
+      {
+      this->Texture->PostRender(ren);
+      }
     }
 
   vtkDebugMacro(<<"Superclass::RenderOverlay called");

Rendering/FreeType/Testing/Cxx/TestFreeTypeTextMapperNoMath.cxx
@@ -172,6 +172,19 @@ int TestFreeTypeTextMapperNoMath(int argc, char *argv[])
   mapper11->SetInput("oTeVaVoVAW");
   actor11->SetPosition(300, 200);
 
+  // Empty string, solid background: should not render
+  vtkNew<vtkTextMapper> mapper12;
+  vtkNew<vtkActor2D> actor12;
+  actor12->SetMapper(mapper12.GetPointer());
+  mapper12->GetTextProperty()->SetFontSize(16);
+  mapper12->GetTextProperty()->SetColor(1.0, 0.0, 0.0);
+  mapper12->GetTextProperty()->SetBackgroundColor(1.0, 0.5, 1.0);
+  mapper12->GetTextProperty()->SetBackgroundOpacity(1.0);
+  mapper12->GetTextProperty()->SetJustificationToRight();
+  mapper12->GetTextProperty()->SetVerticalJustificationToCentered();
+  mapper12->SetInput("");
+  actor12->SetPosition(0, 0);
+
   // Boring rendering setup....
 
   vtkNew<vtkRenderer> ren;
@@ -193,6 +206,7 @@ int TestFreeTypeTextMapperNoMath(int argc, char *argv[])
   ren->AddActor(actor9.GetPointer());
   ren->AddActor(actor10.GetPointer());
   ren->AddActor(actor11.GetPointer());
+  ren->AddActor(actor12.GetPointer());
 
   win->SetMultiSamples(0);
   win->Render();

commit 0b35c3a2255d5f97d38cda6f1c056b8c25249c25

Handle case when textDims are NULL
Add a NULL pointer check to handle the case when textDims are not provided in
vtkFreeTypeTools::RenderStringInternal().

This fixes a regression introduced in commit 5dcad97.

Rendering/FreeType/vtkFreeTypeTools.cxx
@@ -1242,8 +1242,11 @@ bool vtkFreeTypeTools::RenderStringInternal(vtkTextProperty *tprop,
   if (str.empty())
     {
     data->Initialize();
-    textDims[0] = 0;
-    textDims[1] = 0;
+    if (textDims)
+      {
+      textDims[0] = 0;
+      textDims[1] = 0;
+      }
     return true;
     }
 