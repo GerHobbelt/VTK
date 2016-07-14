GerHobbelt/VTK

commit 902356a9d55bdec76b9e95a8c0c68fd56ff0a55c

Fix "same expression in both branches of ternary operator".
Fixes issue caught by cppcheck. Updating the code based on the comment
which suggests the intention of the developer.

IO/Xdmf2/vtkXdmfHeavyData.cxx
@@ -1196,9 +1196,9 @@ vtkDataArray* vtkXdmfHeavyData::ReadAttribute(XdmfAttribute* xmfAttribute,
     if (attrCenter == XDMF_ATTRIBUTE_CENTER_NODE)
       {
       // Point count is 1 + cell extent if not a single layer
-      count[0] += (update_extents[5] - update_extents[4] > 0)? 1 : 1;
-      count[1] += (update_extents[3] - update_extents[2] > 0)? 1 : 1;
-      count[2] += (update_extents[1] - update_extents[0] > 0)? 1 : 1;
+      count[0] += ((update_extents[5] - update_extents[4]) > 0)? 1 : 0;
+      count[1] += ((update_extents[3] - update_extents[2]) > 0)? 1 : 0;
+      count[2] += ((update_extents[1] - update_extents[0]) > 0)? 1 : 0;
       }
     xmfDataItem.GetDataDesc()->SelectHyperSlab(start, stride, count);
     }

commit b32732cb0695e0ef386807290dbc66f34c358b9f

revert a change that broke 2D handling in at least one case
As per ParaView bug report 16781, VTK commit 902356a9 introduced
a regression that broke input of 2D image slice data in at least
one case.

IO/Xdmf2/vtkXdmfHeavyData.cxx
@@ -1196,9 +1196,9 @@ vtkDataArray* vtkXdmfHeavyData::ReadAttribute(XdmfAttribute* xmfAttribute,
     if (attrCenter == XDMF_ATTRIBUTE_CENTER_NODE)
       {
       // Point count is 1 + cell extent if not a single layer
-      count[0] += ((update_extents[5] - update_extents[4]) > 0)? 1 : 0;
-      count[1] += ((update_extents[3] - update_extents[2]) > 0)? 1 : 0;
-      count[2] += ((update_extents[1] - update_extents[0]) > 0)? 1 : 0;
+      count[0] += 1;//((update_extents[5] - update_extents[4]) > 0)? 1 : 0;
+      count[1] += 1;//((update_extents[3] - update_extents[2]) > 0)? 1 : 0;
+      count[2] += 1;//((update_extents[1] - update_extents[0]) > 0)? 1 : 0;
       }
     xmfDataItem.GetDataDesc()->SelectHyperSlab(start, stride, count);
     }